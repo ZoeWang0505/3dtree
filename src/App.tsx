34cb0dfab177a7720308385409b890aae732d4aa
Zoe Wang <zoe.wang0505@gmail.com>
2025-02-26 04:32:56 -0500

    Implement recursion create branches

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
-import { AxesHelper, GridHelper, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
+import { AxesHelper, BoxGeometry, ConeGeometry, CylinderGeometry, GridHelper, Group, MathUtils, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer } from 'three';
 import { createCamera } from './camera';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
+const radiusDefault: number = 0.2;
+
+const angle: number = 60;
+const branchLengthDefault = 20;
+const sigmentNumber = 32;
 const App = () => {
   const containerRef = useRef<HTMLDivElement>(null); 
 
@@ -11,6 +17,10 @@ const App = () => {
   )
   const [camera] = useState<PerspectiveCamera>(createCamera());
   const [scene, setScene] = useState(new Scene());
+  const [branchNumber, setBranchNumber] = useState(6);
+  const [level, setLevel] = useState(2);
+  const [control, setControl] = useState<OrbitControls|null>(null);
+  const [myTree, setMyTree] = useState<Group|Mesh| null>(null);
 
   const newAxesHelper = () => {
     const helper = new AxesHelper(3);
@@ -22,19 +32,58 @@ const App = () => {
     return helper;
   }
 
-  const initScene = () => {
-    scene.add(camera);
-    scene.add(newGridHelper());
-    scene.add(newAxesHelper());
-    rendererRef.current.render(scene, camera);
+  const newOrbitControl = (): OrbitControls =>{
+    const controls = new OrbitControls(camera, rendererRef.current.domElement);
+    controls.minDistance = 1;
+    controls.maxDistance = 95;
+    controls.enablePan = true;
+    // damping & auto rotation require the controls to be updated each frame
+    controls.enableDamping = true;
+    return controls;
   }
+
+  const createBranchs = (depth: number, branchNumber: number, parentLength : number, radius: number): Group => {
+
+    if (depth == 0) {
+      const branch = new Group();
+        const geometry = new CylinderGeometry(radius,radius * 1.2, parentLength, sigmentNumber);
+        const material = new MeshBasicMaterial({ color: 0x00ff00 });
+        const cone = new Mesh(geometry, material);
+        cone.position.y = parentLength /2;
+        branch.add(cone);
+      return branch;
+    }
+
+    const childRadius = radius * 0.4;
+    
+    const childLength = parentLength / 2;
+    const parent = createBranchs(0, branchNumber, parentLength,radius);
+    for (let i = branchNumber; i !== 0; i-=1) {
+      const child = createBranchs(depth - 1, branchNumber, childLength,childRadius);
+      child.rotation.x = MathUtils.degToRad(angle);  
+      child.position.y = parentLength / 6 * i;  
+      parent.add(child);
+    }
+    return parent;
+  }
+
+const initScene = () => {
+  scene.add(camera);
+  scene.add(newGridHelper());
+  scene.add(newAxesHelper());
+  setControl(newOrbitControl());
+
+  const tree = createBranchs(level, branchNumber, branchLengthDefault, radiusDefault);
+  setMyTree(tree);
+  scene.add(tree);
+  camera.position.z = 5;
+}
 
   useEffect(() => {
    const onResize = () => {
     if (containerRef.current !== null) {
-      camera.aspect = containerRef.current.clientWidth / containerRef.current.clientHeight;
       camera.updateProjectionMatrix(); // automatically recalculate the frustrum
-      rendererRef.current.setSize(containerRef.current.clientWidth, containerRef.current.clientHeight);
+      rendererRef.current.setSize(window.innerWidth, window.innerHeight);
       rendererRef.current.setPixelRatio(window.devicePixelRatio);
       rendererRef.current.render(scene, camera);
     }
@@ -45,24 +94,22 @@ const App = () => {
 
    window.addEventListener('resize', onResize);
 
+
+
+  // Animation loop
+  const animate = () => {
+    requestAnimationFrame(animate);
+    rendererRef.current.render(scene, camera);
+  };
+  animate();
+  // Clean up on component unmount
+  return () => {
+    containerRef.current?.removeChild(rendererRef.current.domElement);
+    };
   }, []);
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div className="App-header" ref={containerRef}/>
     </div>
   );
